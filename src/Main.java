990f56f577264ccdfdfd87b550b22c17c705d07b
Cozdig <tank-20@list.ru>
2025-05-17 15:54:53 +0300

    HW ready

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
+        Gryffindor Garry = new Gryffindor("Гарри", "Поттер", 100, 100, 80);
+        System.out.println(Garry);
+        Gryffindor Hermeona = new Gryffindor("Гермиона", "Грейнджер", 90, 100, 85);
+        Hermeona.anyComparison(Garry);
+        Hermeona.studentComparison(Garry);
     }
 }
